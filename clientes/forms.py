e289de42769844484248b7de3cc1b826567342e0
JoBonnemaison <jo.bonnemaison@duocuc.cl>
2024-07-17 00:28:25 -0400

    primer avance en visualizacion de perfil (30%)

--- clientes/forms.py
+++ clientes/forms.py
@@ -6,51 +6,51 @@ class RegistroPerfilForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = [
-            'id_tipo_cliente', 'numero_identificacion', 'nombre', 'segundo_nombre',
-            'apellido', 'segundo_apellido', 'email', 'telefono', 'direccion',
-            'comuna', 'region', 'pais', 'nombre_empresa', 'rut'
+            'id_tipo_cliente','nombre', 'segundo_nombre','apellido', 'segundo_apellido', 'numero_identificacion','nombre_empresa', 'rut',  'email', 'telefono', 'direccion',
+            'comuna', 'region', 'pais'
         ]
         widgets = {            
             'id_tipo_cliente': forms.Select(attrs={'class': 'form-control', 'id': 'tipo_cliente'}),
-            'numero_identificacion': forms.TextInput(attrs={'class': 'form-control'}),
-            'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su nombre'}),
-            'segundo_nombre': forms.TextInput(attrs={'class': 'form-control'}),
-            'apellido': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su apellido'}),
-            'segundo_apellido': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su correo electrónico'}),
-            'telefono': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su teléfono'}),
-            'direccion': forms.TextInput(attrs={'class': 'form-control'}),
+            'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escribe tu nombre'}),
+            'segundo_nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escribe tu segundo nombre'}),
+            'apellido': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escribe tu apellido'}),
+            'segundo_apellido': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escribe tu segundo apellido'}),
+            'numero_identificacion': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escribe tu número de documento de identidad'}),
+            'nombre_empresa': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escribe el nombre de tu empresa'}),
+            'rut': forms.TextInput(attrs={'class': 'form-control','placeholder': 'Escribe el rut de tu empresa'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Escribe tu correo electrónico'}),
+            'telefono': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa tu teléfono'}),
+            'direccion': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingresa tu dirección'}),
             'comuna': forms.Select(attrs={'class': 'form-control'}),
             'region': forms.Select(attrs={'class': 'form-control'}),
-            'pais': forms.Select(attrs={'class': 'form-control'}),
-            'nombre_empresa': forms.TextInput(attrs={'class': 'form-control'}),
-            'rut': forms.TextInput(attrs={'class': 'form-control'})
+            'pais': forms.Select(attrs={'class': 'form-control'})
+           
         }
 
 class PerfilForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = [
-            'id_tipo_cliente', 'numero_identificacion', 'id_user', 'nombre', 'segundo_nombre',
-            'apellido', 'segundo_apellido', 'email', 'telefono', 'direccion',
-            'comuna', 'region', 'pais', 'nombre_empresa', 'rut'
+            'id_tipo_cliente','nombre', 'segundo_nombre','apellido', 'segundo_apellido', 'numero_identificacion','nombre_empresa', 'rut',  'email', 'telefono', 'direccion',
+            'comuna', 'region', 'pais'
         ]
         widgets = {
             'id_tipo_cliente': forms.Select(attrs={'class': 'form-control', 'id': 'tipo_cliente', 'disabled': 'disabled'}),
-            'numero_identificacion': forms.TextInput(attrs={'class': 'form-control', 'disabled': 'disabled'}),
-            'id_user': forms.HiddenInput(),  
+            'id_user': forms.HiddenInput(),
             'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su nombre'}),
             'segundo_nombre': forms.TextInput(attrs={'class': 'form-control'}),
             'apellido': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su apellido'}),
             'segundo_apellido': forms.TextInput(attrs={'class': 'form-control'}),
+            'numero_identificacion': forms.TextInput(attrs={'class': 'form-control', 'disabled': 'disabled'}),
+            'nombre_empresa': forms.TextInput(attrs={'class': 'form-control'}),
+            'rut': forms.TextInput(attrs={'class': 'form-control', 'disabled': 'disabled'}),
             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su correo electrónico'}),
             'telefono': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese su teléfono'}),
             'direccion': forms.TextInput(attrs={'class': 'form-control'}),
             'comuna': forms.Select(attrs={'class': 'form-control'}),
             'region': forms.Select(attrs={'class': 'form-control'}),
-            'pais': forms.Select(attrs={'class': 'form-control'}),
-            'nombre_empresa': forms.TextInput(attrs={'class': 'form-control'}),
-            'rut': forms.TextInput(attrs={'class': 'form-control', 'disabled': 'disabled'})
+            'pais': forms.Select(attrs={'class': 'form-control'})
+            
         }
 
 
